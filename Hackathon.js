0aa7e0dcbf57c62192eb917d011c97523f043186
Puka Kgaogelo <102986562+u21529664@users.noreply.github.com>
2022-05-18 00:50:17 +0200

    Add files via upload

---
+++ Hackathon.js
@@ -0,0 +1,17 @@
+function Close(){
+  window.location.href ="BDO-Hackathon.html";
+  alert("We are now redirecting you to the home page,Thank You!!")
+}
+function ContactFormButton(){
+  window.location.href ="BDO-Hackathon.html";
+  alert("Your review has been sent through,you will receive an email once the problem is sorted.We are now redirecting you to the home page,Thank You!!")
+}
+function Track(){
+  
+  alert("Your funds have been allocated them,so you can track them.We are now redirecting you to the tracking funds page,Thank You!!")
+}
+
+
+
+
+
